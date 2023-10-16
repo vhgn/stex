bd039d3323a4e9d38993c5f6992510144d9a82f2
Vahagn <metavahagn@gmail.com>
2023-10-16 23:54:00 +0400

    feat: add model helper

--- stex_server/src/main.rs
+++ stex_server/src/main.rs
@@ -1,14 +1,51 @@
-use dotenvy::dotenv;
+use anyhow::anyhow;
 use anyhow::Result;
+use dotenvy::dotenv;
+use libsql_client::{args, Client, Statement};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
 
-    let db = libsql_client::Client::from_env().await?;
+    let db = Client::from_env().await?;
 
-    let q = db.execute("select * from users").await?;
-    dbg!(q.rows);
+    let connection = get_domain_connection(db, "exam")
+        .await
+        .ok_or(anyhow!("domain not found"))?;
+
+    dbg!(connection);
 
     Ok(())
+}
+
+#[derive(Debug)]
+struct Connection {
+    host: String,
+    port: String,
+    user: String,
+}
+
+async fn get_domain_connection(db: Client, domain: &str) -> Option<Connection> {
+    let result = db
+        .execute(Statement::with_args(
+            "
+            select host, port, user
+            from domains
+            where domain = ?
+        ",
+            args!(domain),
+        ))
+        .await
+        .ok()?;
+
+    let first = result.rows.first()?;
+    let host = first.values.get(0)?;
+    let port = first.values.get(1)?;
+    let user = first.values.get(2)?;
+
+    Some(Connection {
+        host: host.to_string(),
+        port: port.to_string().parse().ok()?,
+        user: user.to_string(),
+    })
 }
