0395b9f18f0592a783b38336965706648da66f80
Vahagn <metavahagn@gmail.com>
2023-10-16 23:31:17 +0400

    feat: add basic db connection

--- stex_server/src/main.rs
+++ stex_server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+
+    let db = libsql_client::Client::from_env().await?;
+
+    let q = db.execute("select * from users").await?;
+    dbg!(q.rows);
+
+    Ok(())
 }
