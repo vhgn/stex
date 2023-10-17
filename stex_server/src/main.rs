30140017f542c99397454a25746dcf27f09091fa
Vahagn <metavahagn@gmail.com>
2023-10-17 21:32:33 +0400

    fix: use libsql for turso

--- stex_server/src/main.rs
+++ stex_server/src/main.rs
@@ -1,26 +1,28 @@
 use anyhow::Result;
 use dotenvy::dotenv;
 use futures::executor::block_on;
-use sqlx::sqlite::SqlitePoolOptions;
-use sqlx::Pool;
-use sqlx::Sqlite;
-use stex_common::redirect::redirect_stream;
+
+use libsql_client::args;
+use libsql_client::Client;
+use libsql_client::Statement;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
+use stex_common::redirect::redirect_stream;
+
+type Db = Arc<Mutex<Client>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
 
     let listener = TcpListener::bind("0.0.0.0:80")?;
-    let pool = SqlitePoolOptions::new()
-        .max_connections(5)
-        .connect(&std::env::var("DATABASE_URL")?)
-        .await?;
+    let db = Arc::new(Mutex::new(Client::from_env().await?));
     for stream in listener.incoming() {
         if let Ok(stream) = stream {
-            handle_connection(stream, pool.clone());
+            handle_connection(stream, db.clone());
         } else {
             eprintln!("failed to accept connection");
         }
@@ -29,24 +31,25 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-fn handle_connection(incoming_stream: TcpStream, db: Pool<Sqlite>) {
+fn handle_connection(incoming_stream: TcpStream, db: Db) {
     thread::spawn(move || {
+        let db = db.lock().unwrap();
         let host = block_on(get_host_for_domain(&db, "exam")).expect("Failed to get host");
         redirect_stream(incoming_stream, host)
     });
 }
 
-#[derive(sqlx::FromRow)]
-struct Host {
-    host: String,
-}
-
-async fn get_host_for_domain(db: &Pool<Sqlite>, domain: &str) -> Option<String> {
-    let result: Host = sqlx::query_as("select host from domains where domain = ?")
-        .bind(domain)
-        .fetch_one(db)
+async fn get_host_for_domain(db: &Client, domain: &str) -> Option<String> {
+    let result = db
+        .execute(Statement::with_args(
+            "select host from domains where domain = ?",
+            args!(domain),
+        ))
         .await
         .ok()?;
 
-    Some(result.host)
+    let first = result.rows.first()?;
+    let value = first.values.get(0)?;
+
+    Some(value.to_string())
 }
